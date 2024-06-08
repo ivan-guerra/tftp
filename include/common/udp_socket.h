87265c9d5efe9275aea8d0c605615cd83b0b358f
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-06-08 11:19:47 -0400

    Add a UDP sender socket type.

--- include/common/udp_socket.h
+++ include/common/udp_socket.h
@@ -1,12 +1,14 @@
 #ifndef UDP_SOCKET_H_
 #define UDP_SOCKET_H_
 
+#include <netdb.h>
 #include <sys/types.h>
 
 #include <cstddef>
 #include <cstdint>
 #include <expected>
 #include <string>
+#include <string_view>
 
 namespace tftp {
 
@@ -26,7 +28,7 @@ class UdpSocketRecver {
   uint16_t RecvPort() const { return port_; }
   uint16_t LastSenderPort() const { return last_sender_port_; }
 
-  std::expected<ssize_t, UdpSocketErr> RecvFrom(void* buffer, std::size_t len);
+  std::expected<ssize_t, UdpSocketErr> Recv(void* buffer, std::size_t len);
 
   friend void Swap(UdpSocketRecver& r1, UdpSocketRecver& r2);
 
@@ -37,6 +39,42 @@ class UdpSocketRecver {
   int socket_ = -1;
   uint16_t port_ = 0;
   uint16_t last_sender_port_ = 0;
+};
+
+class UdpSocketSender {
+ public:
+  static std::expected<UdpSocketSender, UdpSocketErr> Create(
+      std::string_view ip_addr, uint16_t port);
+
+  ~UdpSocketSender();
+  UdpSocketSender(const UdpSocketSender&) = delete;
+  UdpSocketSender& operator=(const UdpSocketSender&) = delete;
+  UdpSocketSender(UdpSocketSender&&);
+  UdpSocketSender& operator=(UdpSocketSender&&);
+
+  std::string_view IpAddr() const { return ip_addr_; }
+  uint16_t SendPort() const { return port_; }
+
+  std::expected<ssize_t, UdpSocketErr> Send(void* buffer, std::size_t len);
+
+  friend void Swap(UdpSocketSender& r1, UdpSocketSender& r2);
+
+ private:
+  explicit UdpSocketSender(int socket = -1,
+                           std::string_view ip_addr = "127.0.0.1",
+                           uint16_t port = 0, addrinfo* servinfo = nullptr,
+                           addrinfo* addr = nullptr)
+      : socket_(socket),
+        ip_addr_(ip_addr),
+        port_(port),
+        servinfo_(servinfo),
+        addr_(addr) {}
+
+  int socket_ = -1;
+  std::string ip_addr_;
+  uint16_t port_ = 0;
+  addrinfo* servinfo_ = nullptr;
+  addrinfo* addr_ = nullptr;
 };
 
 }  // namespace tftp
--- src/common/udp_socket.cpp
+++ src/common/udp_socket.cpp
@@ -13,6 +13,7 @@
 #include <cstring>
 #include <expected>
 #include <string>
+#include <string_view>
 
 namespace tftp {
 
@@ -20,6 +21,10 @@ UdpSocketRecver::~UdpSocketRecver() {
   if (-1 != socket_) {
     close(socket_);
   }
+
+  socket_ = -1;
+  port_ = 0;
+  last_sender_port_ = 0;
 }
 
 UdpSocketRecver::UdpSocketRecver(UdpSocketRecver&& other) : UdpSocketRecver() {
@@ -36,12 +41,16 @@ std::expected<UdpSocketRecver, UdpSocketErr> UdpSocketRecver::Create(
     uint16_t port, uint32_t timeout_ms) {
   struct addrinfo* servinfo = nullptr;
   struct addrinfo hints = {};
-  hints.ai_family = AF_INET6;
+  hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_DGRAM;
   hints.ai_flags = AI_PASSIVE; /* Use this machine's IP. */
-  if (getaddrinfo(nullptr, std::to_string(port).c_str(), &hints, &servinfo) ==
-      -1) {
+
+  int retcode =
+      getaddrinfo(nullptr, std::to_string(port).c_str(), &hints, &servinfo);
+  if (retcode == -1) {
     return std::unexpected(std::strerror(errno));
+  } else if (retcode) {
+    return std::unexpected(gai_strerror(retcode));
   }
 
   /* Bind to the first address we can. */
@@ -61,6 +70,9 @@ std::expected<UdpSocketRecver, UdpSocketErr> UdpSocketRecver::Create(
     break; /* We were able to construct and bind a socket. */
   }
 
+  if (!it) {
+    return std::unexpected("failed to bind socket");
+  }
   freeaddrinfo(servinfo);
 
   /* Don't wait for the kernel to release the resource. Allow port reuse. */
@@ -70,7 +82,8 @@ std::expected<UdpSocketRecver, UdpSocketErr> UdpSocketRecver::Create(
     return std::unexpected(std::strerror(errno));
   }
 
-  if (timeout_ms) { /* Set a timeout on the socket if one was requested. */
+  /* Set a timeout on the socket if one was requested. */
+  if (timeout_ms) {
     struct timeval tv = {.tv_sec = 1000 * timeout_ms / 1000000,
                          .tv_usec = 1000 * timeout_ms % 1000000};
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,
@@ -83,8 +96,8 @@ std::expected<UdpSocketRecver, UdpSocketErr> UdpSocketRecver::Create(
   return UdpSocketRecver(sockfd, port);
 }
 
-std::expected<ssize_t, UdpSocketErr> UdpSocketRecver::RecvFrom(
-    void* buffer, std::size_t len) {
+std::expected<ssize_t, UdpSocketErr> UdpSocketRecver::Recv(void* buffer,
+                                                           std::size_t len) {
   struct sockaddr_storage sender_addr = {};
   socklen_t addr_len = sizeof(sender_addr);
   ssize_t num_bytes =
@@ -106,7 +119,7 @@ std::expected<ssize_t, UdpSocketErr> UdpSocketRecver::RecvFrom(
     last_sender_port_ =
         ntohs(reinterpret_cast<sockaddr_in*>(&sender_addr)->sin_port);
   } else {
-    std::unexpected("error: protocol is not IPv4 or IPv6");
+    std::unexpected("protocol is not IPv4 or IPv6");
   }
 
   return num_bytes;
@@ -118,6 +131,94 @@ void Swap(UdpSocketRecver& r1, UdpSocketRecver& r2) {
   swap(r1.socket_, r2.socket_);
   swap(r1.port_, r2.port_);
   swap(r1.last_sender_port_, r2.last_sender_port_);
+}
+
+UdpSocketSender::~UdpSocketSender() {
+  if (-1 != socket_) {
+    close(socket_);
+  }
+
+  if (servinfo_) {
+    freeaddrinfo(servinfo_);
+  }
+
+  socket_ = -1;
+  ip_addr_ = "";
+  port_ = 0;
+  servinfo_ = nullptr;
+  addr_ = nullptr;
+}
+
+UdpSocketSender::UdpSocketSender(UdpSocketSender&& other) : UdpSocketSender() {
+  Swap(*this, other);
+}
+
+UdpSocketSender& UdpSocketSender::operator=(UdpSocketSender&& other) {
+  UdpSocketSender tmp(std::move(other));
+  Swap(*this, tmp);
+  return *this;
+}
+
+std::expected<UdpSocketSender, UdpSocketErr> UdpSocketSender::Create(
+    std::string_view ip_addr, uint16_t port) {
+  struct addrinfo* servinfo = nullptr;
+  struct addrinfo hints = {};
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  int retcode = getaddrinfo(ip_addr.data(), std::to_string(port).c_str(),
+                            &hints, &servinfo);
+  if (retcode == -1) {
+    return std::unexpected(std::strerror(errno));
+  } else if (retcode) {
+    return std::unexpected(gai_strerror(retcode));
+  }
+
+  /* Create a socket from the first valid address. */
+  int sockfd = 0;
+  struct addrinfo* it = nullptr;
+  for (it = servinfo; it; it = it->ai_next) {
+    sockfd = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
+    if (-1 == sockfd) {
+      continue;
+    }
+    break;
+  }
+
+  if (!it) {
+    freeaddrinfo(servinfo);
+    return std::unexpected("failed to create socket");
+  }
+
+  /* Don't wait for the kernel to release the resource. Allow port reuse. */
+  int yes = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+    close(sockfd);
+    freeaddrinfo(servinfo);
+    return std::unexpected(std::strerror(errno));
+  }
+
+  return UdpSocketSender(sockfd, ip_addr, port, servinfo, it);
+}
+
+std::expected<ssize_t, UdpSocketErr> UdpSocketSender::Send(void* buffer,
+                                                           std::size_t len) {
+  ssize_t num_bytes = sendto(socket_, reinterpret_cast<char*>(buffer), len, 0,
+                             addr_->ai_addr, addr_->ai_addrlen);
+  if (-1 == num_bytes) {
+    return std::unexpected(std::strerror(errno));
+  }
+  return num_bytes;
+}
+
+void Swap(UdpSocketSender& r1, UdpSocketSender& r2) {
+  using std::swap;
+
+  swap(r1.socket_, r2.socket_);
+  swap(r1.ip_addr_, r2.ip_addr_);
+  swap(r1.port_, r2.port_);
+  swap(r1.servinfo_, r2.servinfo_);
+  swap(r1.addr_, r2.addr_);
 }
 
 }  // namespace tftp
